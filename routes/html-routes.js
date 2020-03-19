96e32bb49ac03176860f090b9333f0fd1769b735
Noisey Turtle <bobbyoldani2@gmail.com>
2020-03-18 20:23:19 -0700

    Testing handlebars

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -24,6 +24,6 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    res.sendFile(path.join(__dirname, "../views/index.html"));
   });
 };
