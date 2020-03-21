c879df594829048536ef0599ae24d88c95c06ad1
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-03-21 13:20:49 -0700

    adding create post

--- public/js/create_post.js
+++ public/js/create_post.js
@@ -1,43 +1,44 @@
 $(document).ready(function () {
-
-    let postForm = $('#post-form');
-      // This file just does a GET request to figure out which user is logged in
-    // and updates the HTML on the page
-    let userEmail = $.get("/api/user_data").then(function (data) {
-        console.log(userEmail);
-    });
-
     let postCategory = $('#select-category');
-    let postTitle = $('#post-title');
-    let postBody = $('#post-body');
+    let postTitle = $('#titleText');
+    let postBody = $('#inputText');
+    let postForm = $('.create-form');
 
-    postForm.on('submit', function(event) {
+
+    postForm.on('submit', function (event) {
         event.preventDefault();
+
+
         let userData = {
-            author: userEmail,
             title: postTitle.val().trim(),
             category: postCategory.val(),
             body: postBody.val().trim()
         };
 
-    // If we have an email and password we run the loginUser function and clear the form
-        createPost(userData.author, userData.title, userData.category, userData.body);
+        // If we have an email and password we run the loginUser function and clear the form
+        createPost(userData.title, userData.category, userData.body);
         postTitle.val('');
         postBody.val('');
 
-        function createPost(author, title, category, body) {
-            $.post('/api/post', {
-                author: author,
-                title: title,
-                category: category,
-                body: body
-            })
-            .then(function() {
-                console.log('post added!')
-            })
-            .catch(function(err) {
-                console.log(err);
-            });
+        function createPost(title, category, body) {
+
+            $.get("/api/user_data")
+                .then(function (email) {
+                    console.log(email);
+                    $.post('/api/post', {
+                        author: email.email,
+                        title: title,
+                        category: category,
+                        body: body
+                    })
+                        .then(function () {
+                            console.log('post added!')
+                        })
+                        .catch(function (err) {
+                            console.log(err);
+                        });
+                });
+
         }
     });
 
