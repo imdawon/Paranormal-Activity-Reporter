eaedca165752ae2c19ecb56372370873867099f0
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-03-21 10:36:47 -0700

    added tables

--- models/post.js
+++ models/post.js
@@ -1,15 +1,15 @@
-module.exports = function (sequelize, DataTypes) {
-    var Post = sequelize.define("Post", {
-        // The email cannot be null, and must be a proper email before creation
-        title: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            unique: true
-        },
-        // The password cannot be null
-        body: {
-            type: DataTypes.STRING,
-            allowNull: false
-        }
-    });
+module.exports = function(sequelize, DataTypes) {
+  var Post = sequelize.define("Post", {
+    // The email cannot be null, and must be a proper email before creation
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    // The password cannot be null
+    body: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
 };
