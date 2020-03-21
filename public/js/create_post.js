42b7f4774f088a92cf7fe4f31617c2e9682c2db8
Natasha Wolfe <natasha.c.wolfe@gmail.com>
2020-03-21 12:41:41 -0700

    added create_post.js

--- public/js/create_post.js
+++ public/js/create_post.js
@@ -1,16 +1,46 @@
-// $(document).ready(function () {
+$(document).ready(function () {
 
-//     let postForm = $('#post-form');
-//       // This file just does a GET request to figure out which user is logged in
-//     // and updates the HTML on the page
-//     let userEmail = $.get("/api/user_data").then(function (data) {
-//         console.log(userEmail);
-//     });
+    let postForm = $('#post-form');
+      // This file just does a GET request to figure out which user is logged in
+    // and updates the HTML on the page
+    let userEmail = $.get("/api/user_data").then(function (data) {
+        console.log(userEmail);
+    });
 
-//     let postCategory = $('#select-category');
+    let postCategory = $('#select-category');
+    let postTitle = $('#post-title');
+    let postBody = $('#post-body');
 
-//     let postTitle = $('#post-title');
+    postForm.on('submit', function(event) {
+        event.preventDefault();
+        let userData = {
+            author: userEmail,
+            title: postTitle.val().trim(),
+            category: postCategory.val(),
+            body: postBody.val().trim()
+        };
+
+    // If we have an email and password we run the loginUser function and clear the form
+        createPost(userData.author, userData.title, userData.category, userData.body);
+        postTitle.val('');
+        postBody.val('');
+
+        function createPost(author, title, category, body) {
+            $.post('/api/post', {
+                author: author,
+                title: title,
+                category: category,
+                body: body
+            })
+            .then(function() {
+                console.log('post added!')
+            })
+            .catch(function(err) {
+                console.log(err);
+            });
+        }
+    });
 
 
 
-// });
+});
