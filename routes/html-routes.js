71ad4aca08e40f3c1a3799ef1044934a524271bb
David <david@thejurne.com>
2020-03-23 19:25:15 -0700

    added filter to list of posts

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -26,7 +26,9 @@ module.exports = function(app) {
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
     db.Posts.findAll().then(result => {
-      res.render(path.join(__dirname, "../views/index.handlebars"), {posts: result.reverse()});
+      let reversedList = result.reverse();
+      let filteredList = reversedList.filter(item => item.category === filterValue);
+      res.render(path.join(__dirname, "../views/index.handlebars"), {posts: filteredList});
     });
   });
 };
